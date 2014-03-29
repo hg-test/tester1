77e79676ddbf9e8760749d97ad88a9880ca555c9
Harsh Grover <hg@newzify.com>
2014-03-28 23:22:19 -0400

    version bump to 1.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       'YOUR_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures tester1'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+# long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.2'
 
